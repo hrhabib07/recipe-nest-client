77d1867e04f2785cb31880c4047d4321f2ff83e4
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-10-13 11:19:27 +0600

    rect quill code has been updated

--- src/components/form/RNTextEditor.tsx
+++ src/components/form/RNTextEditor.tsx
@@ -4,17 +4,17 @@ import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css";
 
 // Dynamically import React Quill to prevent SSR issues
-const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+const ReactQuill = dynamic(
+  () => import("react-quill") as Promise<{ default: React.ComponentType<any> }>,
+  { ssr: false }
+);
 
 // Editor toolbar options with additional features
 const modules = {
   toolbar: [
-    // Formatting options
     [{ bold: true }, { italic: true }, { underline: true }],
-    // List options
-    [{ list: "ordered" }, { list: "bullet" }], // Ordered and unordered lists
-    // Checkbox option
-    [{ list: "check" }], // Checkbox (todo list)
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ list: "check" }],
   ],
 };
 
@@ -22,18 +22,17 @@ interface CustomEditorProps {
   label: string;
   name: string;
   value: string;
-  onChange: (value: string) => void; // Callback to handle value changes
+  onChange: (value: string) => void;
 }
 
 const RNTextEditor = ({ label, name, value, onChange }: CustomEditorProps) => {
   return (
     <div className="flex flex-col gap-2">
       <label className="text-sm font-semibold text-default-600">{label}</label>
-      {/* React Quill Editor */}
       <ReactQuill
         className="text-lg h-[calc(100vh-500px)] border border-default-200 rounded-md p-2 focus:outline-none"
         modules={modules}
-        theme="snow" // Use the snow theme
+        theme="snow"
         value={value}
         onChange={onChange}
       />
